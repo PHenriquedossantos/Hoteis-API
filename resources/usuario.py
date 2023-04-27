6dc35e470c0aa2c5d9ed220bda032cbe31d46c7f
unknown <henrique.ph935@gmail.com>
2023-04-27 09:01:54 -0300

    Cria recurso UserRegiste(Resouce)

--- resources/usuario.py
+++ resources/usuario.py
@@ -3,20 +3,32 @@ from models.usuario import UserModel
 
 
 class User(Resource):
-
+    #/usuarios/{user_id}
     def get(self, user_id):
             user = UserModel.find_user(user_id)
             if user:
                 return user.json()
-            return {'message': 'Hotel not found.'}, 404
+            return {'message': 'User not found.'}, 404
 
 
     def delete(self, user_id):
         user = UserModel.find_user(user_id)
         if user:
-            try:
                 user.delete_user()
-            except:
-                return {'message': 'An error ocurred trying to delete hotel.'}, 500
-            return {'message': 'Hotel deleted.'}
-        return {'message': 'Hotel not found.'}, 404
+                return {'message': '...'}, 500
+        return {'message': '...'}, 404
+
+class UserRegister(Resource):
+      #/cadastro
+      def post(self):
+        atributos = reqparse.RequestParser()
+        atributos.add_argument('login', type=str, required=True, help="The field 'login' cannot be left blank")
+        atributos.add_argument('senha', type=str, required=True, help="The field 'senha' cannot be left blank")
+        dados = atributos.parse_args()
+
+        if UserModel.find_by_login(dados['login']):
+                return {"message": "The login '{}' already exists." .format(dados['login'])}
+
+        user = UserModel(**dados)
+        user.save_user()
+        return {'message': 'User cread successfully!'}, 200
