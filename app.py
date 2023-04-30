4d2377031a4201dd3155dee84314e1b468c35318
unknown <henrique.ph935@gmail.com>
2023-04-30 12:37:35 -0300

    Corrige erros de sintaxe

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
 from resources.hotel import Hoteis, Hotel
 from resources.usuario import User, UserRegister, UserLogin, UserLogout
 from flask_jwt_extended import JWTManager
 from blacklist import BLACKLIST
-from flask import jsonify
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
@@ -19,11 +18,11 @@ def cria_banco():
     banco.create_all()
 
 @jwt.token_in_blocklist_loader
-def verifica_blacklist(token):
+def verifica_blacklist(_, token):
     return token['jti'] in BLACKLIST
 
 @jwt.revoked_token_loader
-def token_de_acesso_invalidado():
+def token_de_acesso_invalidado(_, __):
     return jsonify({'message': 'You have been logged out.'}), 401
 
 
